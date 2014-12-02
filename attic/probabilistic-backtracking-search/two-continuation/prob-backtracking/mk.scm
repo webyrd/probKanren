d558c88307c14152ba9acbd04a6c10b19e603d57
William E. Byrd <webyrd@gmail.com>
2014-12-01 22:52:03 -0700

    moved old code to 'attic' and latest code to 'src'

---
+++ attic/probabilistic-backtracking-search/two-continuation/prob-backtracking/mk.scm
@@ -0,0 +1,480 @@
+;; Vicare doesn't seem to support random of a fixnum--need to use Chez for now.
+
+(define-syntax rhs
+  (syntax-rules ()
+    ((_ x) (cdr x))))
+
+(define-syntax lhs
+  (syntax-rules ()
+    ((_ x) (car x))))
+
+(define-syntax size-s
+  (syntax-rules ()
+    ((_ x) (length x))))
+
+(define-syntax var
+  (syntax-rules ()
+    ((_ x) (vector x))))
+
+(define-syntax var?
+  (syntax-rules ()
+    ((_ x) (vector? x))))
+
+
+(define empty-s '())
+(define empty-sk/c-ls '())
+(define empty-rp-ls '())
+(define empty-c `(,empty-s ,empty-sk/c-ls  ,empty-rp-ls))
+
+(define make-rp list)
+
+(define get-sample
+  (lambda (rp)
+    (car rp)))
+
+(define get-log-density
+  (lambda (rp)
+    (cadr rp)))
+
+(define get-x
+  (lambda (rp)
+    (caddr rp)))
+
+(define get-rest-args
+  (lambda (rp)
+    (cdddr rp)))
+
+(define get-s
+  (lambda (c)
+    (car c)))
+
+(define get-sk/c-ls
+  (lambda (c)
+    (cadr c)))
+
+(define get-rp-ls
+  (lambda (c)
+    (caddr c)))
+
+(define ext-sk/c-ls
+  (lambda (sk c)
+    (let ((s (get-s c))
+          (sk/c-ls (get-sk/c-ls c))
+          (rp-ls (get-rp-ls c)))
+      `(,s ((,sk ,c) . ,sk/c-ls) ,rp-ls))))
+
+(define ext-rp-ls
+  (lambda (rp c)
+    (let ((s (get-s c))
+          (sk/c-ls (get-sk/c-ls c))
+	  (rp-ls (get-rp-ls c)))
+      `(,s ,sk/c-ls ,(cons rp rp-ls)))))
+
+(define update-s
+  (lambda (s c)
+    (let ((sk/c-ls (get-sk/c-ls c))
+	  (rp-ls (get-rp-ls c)))
+      `(,s ,sk/c-ls ,rp-ls))))
+
+
+(define ext-s
+  (lambda (x v s)
+    (cons `(,x . ,v) s)))
+
+(define ext-s-check
+  (lambda (x v s)
+    (cond
+      ((occurs-check x v s) #f)
+      (else (ext-s x v s)))))
+
+(define occurs-check
+  (lambda (x v s)
+    (let ((v (walk v s)))
+      (cond
+        ((var? v) (eq? v x))
+        ((pair? v) 
+         (or 
+           (occurs-check x (car v) s)
+           (occurs-check x (cdr v) s)))
+        (else #f)))))
+
+
+(define walk
+  (lambda (u S)
+    (cond
+      ((and (var? u) (assq u S)) =>
+       (lambda (pr) (walk (rhs pr) S)))
+      (else u))))
+
+(define walk*
+  (lambda (w s)
+    (let ((v (walk w s)))
+      (cond
+        ((var? v) v)
+        ((pair? v)
+         (cons
+           (walk* (car v) s)
+           (walk* (cdr v) s)))
+        (else v)))))
+
+
+(define unify
+  (lambda (u v s)
+    (let ((u (walk u s))
+          (v (walk v s)))
+      (cond
+        ((eq? u v) s)
+        ((var? u) (ext-s-check u v s))
+        ((var? v) (ext-s-check v u s))
+        ((and (pair? u) (pair? v))
+         (let ((s (unify 
+                    (car u) (car v) s)))
+           (and s (unify 
+                    (cdr u) (cdr v) s))))
+        ((equal? u v) s)
+        (else #f)))))
+
+
+(define ==
+  (lambda (u v)
+    (lambda (sk fk c)
+      (let ((s (get-s c)))
+        (let ((s (unify u v s)))
+          (if s
+              (sk fk (update-s s c))
+              (retry fk c)))))))
+
+(define conj
+  (lambda (g1 g2)
+    (lambda (sk fk c)
+      (g1 (lambda (fk^ c^) (g2 sk fk^ c^)) fk c))))
+
+(define conj* (lambda args (conj*-aux args)))
+
+(define conj*-aux
+  (lambda (g*)
+    (cond
+      ((null? g*) succeed)
+      ((null? (cdr g*)) (car g*))
+      (else (conj (car g*) (conj*-aux (cdr g*)))))))
+
+
+(define-syntax fresh
+  (syntax-rules ()
+    [(_ (x* ...) g g* ...)
+     (let ((x* (var 'x*)) ...)
+       (conj* g g* ...))]))
+
+;; TODO
+;;
+;; need to repackage the sk/c list in terms of rp's
+;;
+;; might also need to be able to invoke a special fk if the
+;; constraints introduced result in a low probability trace
+;;
+;; probably need to pass some or all of the sampled values to the
+;; continuation when backtracking above a conde, so we can re-use as
+;; much of the sample information as possible.
+(define-syntax conde
+  (syntax-rules ()
+    [(_ (g0 g0* ...) (g* g** ...) ...)
+     (lambda (sk fk c)
+       (letrec ((sk^ (lambda (fk^ c^)
+                       (let ((g-ls (list (conj* g0 g0* ...)
+                                         (conj* g* g** ...)
+                                         ...)))
+                         (let ((pick (random (length g-ls))))
+                           (let ((g (list-ref g-ls pick)))
+                             (let ((c^ (ext-sk/c-ls sk^ c^)))
+                               (g sk fk^ c^))))))))
+         (sk^ fk c)))]))
+
+#|
+
+;; TODO need to figure out the fixpoint with c
+
+(define-syntax conde
+  (syntax-rules ()
+    [(_ (g0 g0* ...) (g* g** ...) ...)
+     (lambda (sk fk c)
+       (letrec ((sk^ (lambda (fk^ c^)
+                       (let ((g-ls (list (conj* g0 g0* ...)
+                                         (conj* g* g** ...)
+                                         ...)))
+                         (let ((make-conde-sample
+                                (lambda ()
+                                  (let ((pick (random (length g-ls))))
+                                    (let ((g (list-ref g-ls pick)))
+                                      (let ((c^ (ext-sk/c-ls sk^ c^)))
+                                        (g sk fk^ c^))))))
+                               (conde-density
+                                (lambda (x-ignored)
+                                  (log (/ (length g-ls))))))
+                           (let ((rp (make-rp make-conde-sample conde-density 'x-ignore)))
+                             (let ((pick (random (length g-ls))))
+                               (let ((g (list-ref g-ls pick)))
+                                 (let ((c^ (ext-sk/c-ls sk^ c^)))
+                                   (g sk fk^ c^))))))))))
+         (sk^ fk c)))]))
+|#
+
+(define uniform-sample
+  (lambda (lo hi)
+    (let ((samp (+ (random (- hi lo)) lo)))
+      samp)))
+
+(define uniform-log-density
+  (lambda (x lo hi)
+    (log (/ (- hi lo)))))
+
+(define uniform
+  (lambda (lo hi x)
+    (lambda (sk fk c)
+      (let ((rp (make-rp uniform-sample uniform-log-density x lo hi)))
+        (sk fk (ext-rp-ls rp c))))))
+
+
+(define flip-sample
+  (lambda (p)
+    (let ((samp (random 1.0)))
+      (<= samp p))))
+
+(define flip-log-density
+  (lambda (x p)
+    (log (if x p (- 1 p)))))
+
+(define flip
+  (lambda (p x)
+    (lambda (sk fk c)
+      (let ((rp (make-rp flip-sample flip-log-density x p)))
+        (sk fk (ext-rp-ls rp c))))))
+
+;; TODO
+;;
+;; keep track of the extension to the original substitution caused by
+;; sampling
+(define solve-rp-constraints
+  ;; fake goal that runs last in run-mh  
+  (lambda (sk fk c)
+    (printf "solve-rp-constraints c: ~s\n" c)
+    (let loop ((rp-ls (get-rp-ls c))
+               (s (get-s c)))
+      (cond
+        [(null? rp-ls) (sk fk (list (update-s s c)
+				    (get-s c)))]
+        [else
+         (let ((rp (car rp-ls)))
+           (let ((x (get-x rp))
+                 (rest-args (get-rest-args rp)))
+             (let ((x (walk* x s))
+                   (rest-args (walk* rest-args s)))
+               (cond
+                 [(andmap ground? rest-args)
+                  (if (ground? x)
+                      (loop (cdr rp-ls) s)
+                      (let ((sample (get-sample rp)))
+                        (let ((samp (apply sample rest-args)))
+                          (loop (cdr rp-ls) (ext-s x samp s)))))]
+                 [(can-any-rp-can-be-processed? (cdr rp-ls) s)
+                  (loop (append (cdr rp-ls) (list rp)) s)]
+                 [else (error 'solve-rp-constraints "can't make progress with rps")]))))]))))
+
+(define can-any-rp-can-be-processed?
+  (lambda (rp-ls s)
+    (let loop ((rp-ls rp-ls))
+      (cond
+        [(null? rp-ls) #f]
+        [(let ((rp (car rp-ls)))
+           (let ((rest-args (get-rest-args rp)))
+             (let ((rest-args (walk* rest-args s)))
+               (andmap ground? rest-args))))
+         #t]
+        [else (loop (cdr rp-ls))]))))
+
+(define ground?
+  (lambda (t)
+    (cond
+      [(var? t) #f]
+      [(pair? t)
+       (and (ground? (car t)) (ground? (cdr t)))]
+      [else #t])))
+
+
+(define retry
+  (lambda (fk c)
+    (let ((sk/c-ls (get-sk/c-ls c)))
+      (if (null? sk/c-ls)
+          (fk)
+          (let ((pick (random (length sk/c-ls))))
+            (let ((sk/c (list-ref sk/c-ls pick)))
+              (let ((sk (car sk/c))
+                    (c (cadr sk/c)))
+                (sk fk c))))))))
+
+(define find-rp-info
+  (lambda (x rp-ls)
+    (cond
+      [(null? rp-ls) (error 'find-rp-info "could not find rp")]
+      [(eq? (caddr (car rp-ls)) x)
+       (car rp-ls)]
+      [else (find-rp-info x (cdr rp-ls))])))
+
+(define resample
+  (lambda (s-prefix fk c/old-s)
+    (let ((c (car c/old-s))
+	  (old-s (cadr c/old-s)))
+      (cond
+       [(null? s-prefix) c/old-s]
+       [else
+	(let ((index-s-prefix (random (length s-prefix))))
+	  (let ((pr (list-ref s-prefix index-s-prefix)))
+	    (let ((new-s-prefix (remq pr s-prefix)))
+	      (let ((rp-ls (get-rp-ls c)))
+                (let ((x (car pr)))
+                  (let ((rp-info (find-rp-info x rp-ls)))
+                    (let ((resample-proc (car rp-info))
+                          (args (cdddr rp-info)))
+                      (let ((val (apply resample-proc (walk* args (append new-s-prefix old-s)))))
+                        (let ((s (append (cons (cons x val) new-s-prefix) old-s)))
+                          (list (update-s s c)
+                                old-s))))))))))]))))
+
+
+(define-syntax run*
+  (syntax-rules ()
+    [(_ (x) g g* ...)
+     (let ((x (var 'x)))
+       ((fresh () g g* ...)
+        (lambda (fk c)
+          (cons (reify x (get-s c)) (fk)))
+        (lambda () '())
+        empty-c))]))
+
+(define-syntax run
+  (syntax-rules ()
+    [(_ ne (x) g g* ...)
+     (let ((n ne)
+           (x (var 'x)))
+       (let ((ans ((fresh () g g* ...)
+                   (lambda (fk c)
+                     (list fk c))
+                   (lambda () '())
+                   empty-c)))
+         (let loop ((n n)
+                    (ans ans)
+                    (ls '()))
+           (cond
+             ((zero? n) (reverse ls))
+             ((null? ans) (reverse ls))
+             (else
+              (let ((fk (car ans))
+                    (c (cadr ans)))
+                (let ((s (get-s c)))
+                  (loop
+                    (sub1 n)
+                    (retry fk c)
+                    (cons (reify x s) ls)))))))))]))
+
+(define get-subst-prefix
+  (lambda (s-old s-new)
+    (cond
+     [(eq? s-old s-new) '()]
+     [else (cons (car s-new)
+		 (get-subst-prefix s-old
+				   (cdr s-new)))])))
+;; TODO
+;;
+;; needs to do resampling: keeping track of the extension to the
+;; original substitution produced by sampling in solve-rp-constraints
+;;
+;; calculate log densities of rps to figure out whether to keep the
+;; old substitution or the new substitution to answer the query
+(define-syntax run-mh
+  (syntax-rules ()
+    [(_ ne (x) g g* ...)
+     (let ((n ne)
+           (x (var 'x)))
+       (let ((ans ((fresh () g g* ... solve-rp-constraints)
+                   (lambda (fk c)
+                     (list fk c))
+                   (lambda () '())
+                   empty-c)))
+         (let loop ((n n)
+                    (ans ans)
+                    (ls '()))
+           (cond
+             ((zero? n) (reverse ls))
+             ((null? ans) (reverse ls))
+             (else
+              (let ((fk (car ans))
+                    (c/old-s (cadr ans)))
+		(let ((c (car c/old-s))
+		      (old-s (cadr c/old-s)))
+		  (let ((s (get-s c)))
+		    (let ((s-prefix (get-subst-prefix old-s s)))
+		      (loop
+		       (sub1 n)
+                       (let ((c/old-s^ (resample s-prefix fk c/old-s)))
+                         (if (reject-sample? c/old-s^ c/old-s)
+                             (list fk c/old-s)     ;; do we really need this fk?
+                             (list fk c/old-s^)))  ;; do we really need this fk?
+		       (cons (reify x s) ls)))))))))))]))
+
+(define reject-sample?
+  (lambda (c/old-s^ c/old-s)
+    (let ((c^ (car c/old-s^))
+          (old-s^ (cadr c/old-s^))
+          (c (car c/old-s))
+	  (old-s (cadr c/old-s)))
+      (let ((rp-ls^ (get-rp-ls c^))
+            (rp-ls (get-rp-ls c)))
+        (let ((mproc (lambda (c)
+                       (lambda (rp-info)
+                         (let ((proc (cadr rp-info))
+                               (x/args (cddr rp-info)))
+                           (apply proc (walk* x/args (get-s c))))))))
+          (let ((ll^ (apply + (map (mproc c^) rp-ls^)))
+                (ll (apply + (map (mproc c) rp-ls))))
+            (let ((u (random 1.0)))
+              (> (log u)
+                 ;; TODO will need another term and some bookkeeping here
+                 (+ (- ll^ ll) (- (length rp-ls^) (length rp-ls)))))))))))
+
+(define reify-s
+  (lambda (v s)
+    (let ((v (walk v s)))
+      (cond
+        ((var? v)
+         (ext-s v (reify-name (size-s s)) s))
+        ((pair? v) (reify-s (cdr v)
+                     (reify-s (car v) s)))
+        (else s)))))
+
+(define reify-name
+  (lambda (n)
+    (string->symbol
+      (string-append "_" "." (number->string n)))))
+
+(define reify
+  (lambda (v s)
+    (let ((v (walk* v s)))
+      (walk* v (reify-s v empty-s)))))
+
+(define succeed
+  (lambda (sk fk c)
+    (sk fk c)))
+
+(define fail
+  (lambda (sk fk c)
+    (fk)))
+
+
+;; interesting test--what does this mean?
+;;
+;; (run 1 (q) (flip 0.00001 q) (flip 0.999999 q))
+;;
+;; vs.
+;;
+;; (run 1 (q) (flip 0.999999 q) (flip 0.00001 q))
+;;
+;; Are these equivalent?  Does this make sense?
---
+++ attic/probabilistic-backtracking-search/two-continuation/prob-backtracking/mktests.scm
@@ -0,0 +1,388 @@
+(load "mk.scm")
+(load "test-check.scm")
+
+;; Example run can be found in comments for the non-deterministic
+;; tests below
+
+(test "1a"
+  (run 1 (q) (== q 5))
+  '(5))
+
+(test "1b"
+  (run 2 (q) (== q 5))
+  '(5))
+
+(test "1c"
+  (run* (q) (== q 5))
+  '(5))
+
+(test "2a"
+  (run 1 (q) (== q 5) (== q 6))
+  '())
+
+(test "2b"
+  (run* (q) (== q 5) (== q 6))
+  '())
+
+(test "3a"
+  (run 1 (q) (== q 5) (== q 5))
+  '(5))
+
+(test "3b"
+  (run 2 (q) (== q 5) (== q 5))
+  '(5))
+
+(test "3c"
+  (run* (q) (== q 5) (== q 5))
+  '(5))
+
+(test "4a"
+  (run 1 (q)
+    (conde
+      ((== q 5))
+      ((== q 5))))
+  '(5))
+
+(test "4b"
+  (run 2 (q)
+    (conde
+      ((== q 5))
+      ((== q 5))))
+  '(5 5))
+
+(test "4c"
+  (run 5 (q)
+    (conde
+      ((== q 5))
+      ((== q 5))))
+  '(5 5 5 5 5))
+
+(test "5a"
+  (let ((val (run 1 (q)
+               (conde
+                 ((== q 5))
+                 ((== q 6))))))
+    (printf "val: ~s\n" val)
+    (and
+      (= (length val) 1)
+      (for-all (lambda (v) (or (= 5 v) (= 6 v))) val)))
+  #t)
+;; val: (5)
+
+(test "5b"
+  (let ((val (run 10 (q)
+               (conde
+                 ((== q 5))
+                 ((== q 6))))))
+    (printf "val: ~s\n" val)
+    (and
+      (= (length val) 10)
+      (for-all (lambda (v) (or (= 5 v) (= 6 v))) val)))
+  #t)
+;; val: (5 6 6 5 6 6 5 5 5 5)
+
+(test "5c"
+  (let ((val (run 100 (q)
+               (conde
+                 ((== q 5))
+                 ((== q 6))))))
+    (printf "val: ~s\n" val)
+    (and
+      (= (length val) 100)
+      (for-all (lambda (v) (or (= 5 v) (= 6 v))) val)))
+  #t)
+;; val: (5 6 6 5 6 6 6 5 5 6 6 5 6 6 6 5 6 5 6 6 6 5 6 6 5 5 6 6 5 6 6 5 5 6 5 6 5 5 6 5 5 5 5 6 5 6 5 6 5 5 5 5 6 5 6 6 6 6 5 5 5 6 6 5 5 5 5 5 5 6 6 5 6 6 5 6 6 5 6 6 6 5 6 5 5 6 5 6 6 5 5 5 6 5 6 6 5 6 6 5)
+
+(test "6a"
+  (let ((val (run 10 (q)
+               (fresh (x y)
+                 (conde
+                   ((== x 1))
+                   ((== x 2)))
+                 (conde
+                   ((== y 3))
+                   ((== y 4)))
+                 (== (list x y) q)))))
+    (printf "val: ~s\n" val)
+    (not (not (and
+               (= (length val) 10)
+               (for-all (lambda (x/y) (let ((x (car x/y)) (y (cadr x/y))) (and (member x '(1 2)) (member y '(3 4))))) val)))))
+  #t)
+;; val: ((2 3) (2 4) (2 4) (2 3) (2 4) (2 3) (2 4) (2 4) (2 4) (2 3))
+
+(test "6b"
+  (let ((val (run 10 (q)
+               (fresh (x y)
+                 (conde
+                   ((== x 1))
+                   ((== x 2)))
+                 (== (list x y) q)
+                 (conde
+                   ((== y 3))
+                   ((== y 4)))))))
+    (printf "val: ~s\n" val)
+    (not (not (and
+               (= (length val) 10)
+               (for-all (lambda (x/y) (let ((x (car x/y)) (y (cadr x/y))) (and (member x '(1 2)) (member y '(3 4))))) val)))))
+  #t)
+;; val: ((1 3) (1 3) (1 4) (2 4) (2 3) (2 3) (2 4) (2 4) (1 4) (1 3))
+
+(test "6c"
+  (let ((val (run 10 (q)
+               (fresh (x y)
+                 (== (list x y) q)
+                 (conde
+                   ((== x 1))
+                   ((== x 2)))
+                 (conde
+                   ((== y 3))
+                   ((== y 4)))))))
+    (printf "val: ~s\n" val)
+    (not (not (and
+               (= (length val) 10)
+               (for-all (lambda (x/y) (let ((x (car x/y)) (y (cadr x/y))) (and (member x '(1 2)) (member y '(3 4))))) val)))))
+  #t)
+;; val: ((1 3) (1 4) (1 3) (2 4) (2 4) (2 4) (2 3) (2 4) (2 3) (1 4))
+
+(test "6d"
+  (let ((val (run 10 (q)
+               (fresh (x y)
+                 (== (list x y) q)
+                 (conde
+                   ((== y 3))
+                   ((== y 4)))
+                 (conde
+                   ((== x 1))
+                   ((== x 2)))))))
+    (printf "val: ~s\n" val)
+    (not (not (and
+               (= (length val) 10)
+               (for-all (lambda (x/y) (let ((x (car x/y)) (y (cadr x/y))) (and (member x '(1 2)) (member y '(3 4))))) val)))))
+  #t)
+;; val: ((1 4) (2 3) (1 3) (2 4) (1 4) (1 3) (2 3) (1 3) (2 3) (1 3))
+
+(test "7a"
+  (let ((val (run 10 (q)
+               (fresh (x y)
+                 (conde
+                   ((== x 1))
+                   ((== x 2)))
+                 (== (list x y) q)
+                 (conde
+                   ((== y 3))
+                   ((== y 4)))))))
+    (printf "val: ~s\n" val)
+    (not (not (and
+               (= (length val) 10)
+               (for-all (lambda (x/y) (let ((x (car x/y)) (y (cadr x/y))) (and (member x '(1 2)) (member y '(3 4))))) val)))))
+  #t)
+;; val: ((1 3) (1 3) (1 4) (1 3) (1 3) (1 3) (1 3) (1 4) (1 3) (2 3))
+
+(test "8a"
+  (let ((val (run 10 (q)
+               (fresh (x y)
+                 (== (list x y) q)
+                 (conde
+                   ((== x 1))
+                   ((== x 2)))
+                 (conde
+                   ((== y 3))
+                   ((== y 4)))))))
+    (printf "val: ~s\n" val)
+    (not (not (and
+               (= (length val) 10)
+               (for-all (lambda (x/y) (let ((x (car x/y)) (y (cadr x/y))) (and (member x '(1 2)) (member y '(3 4))))) val)))))
+  #t)
+;; val: ((2 3) (1 3) (1 3) (2 4) (2 3) (2 4) (2 4) (2 4) (2 3) (1 3))
+
+(test "9a"
+  (let ((val (run 10 (q)
+               (conde
+                 ((conde
+                    ((== q 1))
+                    ((== q 2))))
+                 ((conde
+                    ((== q 3))
+                    ((== q 4))))))))
+    (printf "val: ~s\n" val)
+    (not (not (and
+               (= (length val) 10)
+               (for-all (lambda (q) (member q '(1 2 3 4))) val)))))
+  #t)
+;; val: (2 1 2 2 2 1 1 2 1 2)
+
+(test "10a"
+  (let ((val (run 10 (q)
+               (fresh (x)
+                 (== x q)
+                 (conde
+                   ((== x 1))
+                   ((== x 2)))))))
+    (printf "val: ~s\n" val)
+    (not (not (and
+               (= (length val) 10)
+               (for-all (lambda (q) (member q '(1 2))) val)))))
+  #t)
+;; val: (1 2 1 2 1 2 2 1 1 1)
+
+(test "11a"
+  (let ((val (run 50 (q)
+               (conde
+                 ((conde
+                    ((conde
+                       ((== q 1))
+                       ((== q 2))))
+                    ((conde
+                       ((== q 3))
+                       ((== q 4))))))
+                 ((conde
+                    ((conde
+                       ((== q 5))
+                       ((== q 6))))
+                    ((conde
+                       ((== q 7))
+                       ((== q 8))))))))))
+    (printf "val: ~s\n" val)
+    (not (not (and
+               (= (length val) 50)
+               (for-all (lambda (q) (member q '(1 2 3 4 5 6 7 8))) val)))))
+  #t)
+;; val: (3 4 2 2 4 3 3 2 2 8 3 4 3 3 8 7 7 6 5 8 7 1 6 1 4 3 4 3 3 1 2 1 1 5 8 7 7 7 8 6 4 3 3 6 7 8 7 5 6 6)
+
+(test "12a"
+  (let ((val (run 20 (q)
+               (conde
+                 ((== q 1))
+                 ((== q 2))
+                 ((== q 3))
+                 ((== q 4))
+                 ((== q 5))
+                 ((== q 6))))))
+    (printf "val: ~s\n" val)
+    (and
+      (= (length val) 20)
+      (not (not (for-all (lambda (q) (member q '(1 2 3 4 5 6))) val)))))
+  #t)
+;; val: (2 1 5 3 6 1 2 3 2 2 1 5 6 6 4 5 5 4 6 3)
+
+(test "13a"
+  (let ((val (run 20 (q)
+               (conde
+                 ((conde
+                    ((== q 1))
+                    ((== q 2))
+                    ((== q 3))))
+                 ((conde
+                    ((== q 4))
+                    ((== q 5))
+                    ((== q 6))))))))
+    (printf "val: ~s\n" val)
+    (and
+     (= (length val) 20)
+     (not (not (for-all (lambda (q) (member q '(1 2 3 4 5 6))) val)))))
+  #t)
+;; val: (2 3 2 3 3 3 3 5 1 5 4 4 6 4 5 5 6 4 3 3)
+
+(test "14a"
+  (let ((val (run 20 (q)
+               (conde
+                 ((conde
+                    ((== q 1))
+                    ((== q 2))))
+                 ((conde                    
+                    ((== q 3))
+                    ((== q 4))))
+                 ((conde                    
+                    ((== q 5))
+                    ((== q 6))))))))
+    (printf "val: ~s\n" val)
+    (and
+     (= (length val) 20)
+     (not (not (for-all (lambda (q) (member q '(1 2 3 4 5 6))) val)))))
+  #t)
+;; val: (1 2 5 5 6 5 6 5 6 6 5 6 5 5 5 4 4 1 1 2)
+
+(define one-or-two
+  (lambda (x)
+    (conde
+      ((== 1 x))
+      ((== 2 x)))))
+
+(test "15a"
+  (let ((val (run 20 (q)
+               (one-or-two q))))
+    (printf "val: ~s\n" val)
+    (and
+     (= (length val) 20)
+     (not (not (for-all (lambda (q) (member q '(1 2))) val)))))
+  #t)
+;; val: (1 1 1 1 1 1 2 2 1 1 2 1 1 2 1 1 2 2 1 1)
+
+(define geom 
+  (lambda (x n)
+    (conde
+      ((== n x))
+      ((geom x (add1 n))))))
+
+(test "16a"
+  (let ((val (run 50 (q)
+               (geom q 0))))
+    (printf "val: ~s\n" val)
+    (and
+     (= (length val) 50)
+     (for-all (lambda (q) (and (integer? q) (>= q 0))) val)))
+  #t)
+;; val: (0 0 2 0 0 0 0 0 0 0 0 2 4 2 2 1 3 0 0 0 9 6 7 4 3 1 0 1 1 0 1 3 1 1 1 2 1 1 3 1 1 1 1 2 2 2 2 7 5 5)
+
+(define mean
+  (lambda (ls)
+    (exact->inexact (/ (apply + ls) (length ls)))))
+
+
+;;; both should be around 1.0
+(mean (apply append (map (lambda (?) (run 1 (q) (geom q 0))) (make-list 10000))))
+
+(mean (run 10000 (q) (geom q 0)))
+
+
+
+
+(define geom2/3
+  (lambda (x n)
+    (conde
+      ((== n x))
+      ((== n x))
+      ((geom2/3 x (add1 n))))))
+
+(mean (run 1000 (q) (geom2/3 q 0)))
+
+(define geom2/3b
+  (lambda (x n)
+    (conde
+      ((== n x))      
+      ((geom2/3b x (add1 n)))
+      ((geom2/3b x (add1 n))))))
+
+(mean (run 1000 (q) (geom2/3b q 0)))
+
+
+
+(run-mh 1 (q) (== #f #f))
+
+(run-mh 2 (q) (== #f #f))
+
+(run-mh 100 (q) (== #f #f))
+
+
+(run-mh 1 (q) (== 0.5 q) (flip q #t))
+
+(run-mh 2 (q) (== 0.5 q) (flip q #t))
+
+(run-mh 100 (q) (== 0.5 q) (flip q #t))
+
+
+(run-mh 1 (q) (fresh (r) (uniform 0.0 0.4 r) (uniform r 1.0 q) (fresh (x) (== x #t) (flip q x))))
+
+(run-mh 2 (q) (fresh (r) (uniform 0.0 0.4 r) (uniform r 1.0 q) (fresh (x) (== x #t) (flip q x))))
+
+(run-mh 100 (q) (fresh (r) (uniform 0.0 0.4 r) (uniform r 1.0 q) (fresh (x) (== x #t) (flip q x))))
