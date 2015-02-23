aa08cd73f22c3db883ecfa4a7aaaa347a6c13867
Rob Zinkov <rob@zinkov.com>
2015-02-23 14:40:15 -0500

    Make more tests run

--- src/mktests.scm
+++ src/mktests.scm
@@ -357,68 +357,62 @@
 
 		   (== q (list bob sue))))))
 
-#!eof
-
 ;; run and run* are now deprecated
 
 ;; Example run can be found in comments for the non-deterministic
 ;; tests below
 
 (test "1a"
-  (run 1 (q) (== q 5))
+  (run-mh 1 (q) (== q 5))
   '(5))
 
 (test "1b"
-  (run 2 (q) (== q 5))
-  '(5))
-
-(test "1c"
-  (run* (q) (== q 5))
-  '(5))
+  (run-mh 2 (q) (== q 5))
+  '(5 5))
 
 (test "2a"
-  (run 1 (q) (== q 5) (== q 6))
+  (run-mh 1 (q) (== q 5) (== q 6))
   '())
 
 (test "2b"
-  (run* (q) (== q 5) (== q 6))
+  (run-mh 1 (q) (== q 5) (== q 6))
   '())
 
 (test "3a"
-  (run 1 (q) (== q 5) (== q 5))
+  (run-mh 1 (q) (== q 5) (== q 5))
   '(5))
 
 (test "3b"
-  (run 2 (q) (== q 5) (== q 5))
-  '(5))
+  (run-mh 2 (q) (== q 5) (== q 5))
+  '(5 5))
 
 (test "3c"
-  (run* (q) (== q 5) (== q 5))
+  (run-mh 1 (q) (== q 5) (== q 5))
   '(5))
 
 (test "4a"
-  (run 1 (q)
+  (run-mh 1 (q)
     (conde
       ((== q 5))
       ((== q 5))))
   '(5))
 
 (test "4b"
-  (run 2 (q)
+  (run-mh 2 (q)
     (conde
       ((== q 5))
       ((== q 5))))
   '(5 5))
 
 (test "4c"
-  (run 5 (q)
+  (run-mh 5 (q)
     (conde
       ((== q 5))
       ((== q 5))))
   '(5 5 5 5 5))
 
 (test "5a"
-  (let ((val (run 1 (q)
+  (let ((val (run-mh 1 (q)
                (conde
                  ((== q 5))
                  ((== q 6))))))
@@ -430,7 +424,7 @@
 ;; val: (5)
 
 (test "5b"
-  (let ((val (run 10 (q)
+  (let ((val (run-mh 10 (q)
                (conde
                  ((== q 5))
                  ((== q 6))))))
@@ -442,7 +436,7 @@
 ;; val: (5 6 6 5 6 6 5 5 5 5)
 
 (test "5c"
-  (let ((val (run 100 (q)
+  (let ((val (run-mh 100 (q)
                (conde
                  ((== q 5))
                  ((== q 6))))))
@@ -454,7 +448,7 @@
 ;; val: (5 6 6 5 6 6 6 5 5 6 6 5 6 6 6 5 6 5 6 6 6 5 6 6 5 5 6 6 5 6 6 5 5 6 5 6 5 5 6 5 5 5 5 6 5 6 5 6 5 5 5 5 6 5 6 6 6 6 5 5 5 6 6 5 5 5 5 5 5 6 6 5 6 6 5 6 6 5 6 6 6 5 6 5 5 6 5 6 6 5 5 5 6 5 6 6 5 6 6 5)
 
 (test "6a"
-  (let ((val (run 10 (q)
+  (let ((val (run-mh 10 (q)
                (fresh (x y)
                  (conde
                    ((== x 1))
@@ -471,7 +465,7 @@
 ;; val: ((2 3) (2 4) (2 4) (2 3) (2 4) (2 3) (2 4) (2 4) (2 4) (2 3))
 
 (test "6b"
-  (let ((val (run 10 (q)
+  (let ((val (run-mh 10 (q)
                (fresh (x y)
                  (conde
                    ((== x 1))
@@ -488,7 +482,7 @@
 ;; val: ((1 3) (1 3) (1 4) (2 4) (2 3) (2 3) (2 4) (2 4) (1 4) (1 3))
 
 (test "6c"
-  (let ((val (run 10 (q)
+  (let ((val (run-mh 10 (q)
                (fresh (x y)
                  (== (list x y) q)
                  (conde
@@ -505,7 +499,7 @@
 ;; val: ((1 3) (1 4) (1 3) (2 4) (2 4) (2 4) (2 3) (2 4) (2 3) (1 4))
 
 (test "6d"
-  (let ((val (run 10 (q)
+  (let ((val (run-mh 10 (q)
                (fresh (x y)
                  (== (list x y) q)
                  (conde
@@ -522,7 +516,7 @@
 ;; val: ((1 4) (2 3) (1 3) (2 4) (1 4) (1 3) (2 3) (1 3) (2 3) (1 3))
 
 (test "7a"
-  (let ((val (run 10 (q)
+  (let ((val (run-mh 10 (q)
                (fresh (x y)
                  (conde
                    ((== x 1))
@@ -539,7 +533,7 @@
 ;; val: ((1 3) (1 3) (1 4) (1 3) (1 3) (1 3) (1 3) (1 4) (1 3) (2 3))
 
 (test "8a"
-  (let ((val (run 10 (q)
+  (let ((val (run-mh 10 (q)
                (fresh (x y)
                  (== (list x y) q)
                  (conde
@@ -556,7 +550,7 @@
 ;; val: ((2 3) (1 3) (1 3) (2 4) (2 3) (2 4) (2 4) (2 4) (2 3) (1 3))
 
 (test "9a"
-  (let ((val (run 10 (q)
+  (let ((val (run-mh 10 (q)
                (conde
                  ((conde
                     ((== q 1))
@@ -572,7 +566,7 @@
 ;; val: (2 1 2 2 2 1 1 2 1 2)
 
 (test "10a"
-  (let ((val (run 10 (q)
+  (let ((val (run-mh 10 (q)
                (fresh (x)
                  (== x q)
                  (conde
@@ -586,7 +580,7 @@
 ;; val: (1 2 1 2 1 2 2 1 1 1)
 
 (test "11a"
-  (let ((val (run 50 (q)
+  (let ((val (run-mh 50 (q)
                (conde
                  ((conde
                     ((conde
@@ -610,7 +604,7 @@
 ;; val: (3 4 2 2 4 3 3 2 2 8 3 4 3 3 8 7 7 6 5 8 7 1 6 1 4 3 4 3 3 1 2 1 1 5 8 7 7 7 8 6 4 3 3 6 7 8 7 5 6 6)
 
 (test "12a"
-  (let ((val (run 20 (q)
+  (let ((val (run-mh 20 (q)
                (conde
                  ((== q 1))
                  ((== q 2))
@@ -626,7 +620,7 @@
 ;; val: (2 1 5 3 6 1 2 3 2 2 1 5 6 6 4 5 5 4 6 3)
 
 (test "13a"
-  (let ((val (run 20 (q)
+  (let ((val (run-mh 20 (q)
                (conde
                  ((conde
                     ((== q 1))
@@ -644,7 +638,7 @@
 ;; val: (2 3 2 3 3 3 3 5 1 5 4 4 6 4 5 5 6 4 3 3)
 
 (test "14a"
-  (let ((val (run 20 (q)
+  (let ((val (run-mh 20 (q)
                (conde
                  ((conde
                     ((== q 1))
@@ -664,7 +658,7 @@
 
 
 (test "15a"
-  (let ((val (run 20 (q)
+  (let ((val (run-mh 20 (q)
                (one-or-two q))))
     (printf "val: ~s\n" val)
     (and
@@ -674,7 +668,7 @@
 ;; val: (1 1 1 1 1 1 2 2 1 1 2 1 1 2 1 1 2 2 1 1)
 
 (test "16a"
-  (let ((val (run 50 (q)
+  (let ((val (run-mh 50 (q)
                (geom q 0))))
     (printf "val: ~s\n" val)
     (and
