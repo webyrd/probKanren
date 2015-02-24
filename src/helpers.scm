186d55cd2bc89733b4912fce7fbb2b923d14f82a
William E. Byrd <webyrd@gmail.com>
2015-02-23 19:44:06 -0700

    updated helper relations

--- src/helpers.scm
+++ src/helpers.scm
@@ -10,19 +10,58 @@
 
 (define seq-goalo
   (lambda (f ls)
-    (conj*-aux (map f ls))))
+    (conde
+      ((== '() ls))
+      ((fresh (a d)
+         (== `(,a . ,d) ls)
+         (f a)
+         (seq-goalo f d))))))
 
+(define map-goalo
+  (lambda (f ls out)
+    (conde
+      ((== '() ls) (== '() out))
+      ((fresh (a d v res)
+         (== `(,a . ,d) ls)
+         (== `(,v . ,res) out)
+         (f x v)
+         (map-goalo f d res))))))
+
+(define zipwitho
+  (lambda (f l s out)
+    (conde
+      [(== '() l) (== '() s) (== '() out)]
+      [(fresh (al dl as ds v res)
+         (== `(,al . ,dl) l)
+         (== `(,as . ,ds) s)
+         (== `(,v . ,res) out)
+         (f al as v)
+         (zipwitho f dl ds res))])))
+
+
+
+;;; old and busted
+#|
+(define seq-goalo
+  (lambda (f ls)
+    (conj*-aux (map f ls))))
+|#
+
+#|
 (define map-goalo
   (lambda (f ls g)
     (conj*-aux (map (lambda (x)
 		      (f x q)) ls))))
+|#
 
+#|
 (define zipwitho
   (lambda (f a b g)
     (cond
-     [(null? a) (== g '())]
-     [else
-      (fresh (x xs)
-	(f (car a) (car b) res)
-	(zipwitho f (cdr a) (cdr b) xs)
-	(== (cons x xs) g))])))
+      [(null? a) (== '() g)]
+      [else
+       (fresh (x xs)
+         (f (car a) (car b) res)
+         (zipwitho f (cdr a) (cdr b) xs)
+         (== (cons x xs) g))])))
+|#
