a2879e4bd926a992f214a620abaa1d33488a06b6
William E. Byrd <webyrd@gmail.com>
2015-06-03 13:36:24 -0600

    added delayed goal version of simple miniKanren

---
+++ delayed-goal-tests.scm
@@ -0,0 +1,24 @@
+(define +o
+  (lambda (x y z)
+    (delayed-goal `(,x ,y)
+      (project (x y z)
+        (== (+ x y) z)))))
+
+(define *o
+  (lambda (x y z)
+    (delayed-goal `(,x ,y)
+      (project (x y z)
+        (== (* x y) z)))))
+
+(define /o
+  (lambda (x y z)
+    (delayed-goal `(,x ,y)
+      (project (x y z)
+        (== (/ x y) z)))))
+
+(define >o
+  (lambda (x y z)
+    (delayed-goal `(,x ,y)
+      (project (x y z)
+        (== (> x y) z)))))
+
