5a5b7bde731fefb7d4c65dafc2464291d78a7833
Rob Zinkov <rob@zinkov.com>
2015-06-04 21:23:25 -0400

    Split prog2 into unconditioned and conditioned case

--- transform.scm
+++ transform.scm
@@ -50,7 +50,28 @@
       (normal 0.0 1.0 x)
       (normal x 1.0 q))))
 
+(define prog2-c
+  (lambda (q x)
+    (fresh ()
+      (== 2.0 x)
+      (normal 0.0 1.0 x)
+      (normal x 1.0 q))))
+
+(define prog2
+  (lambda (q x)
+    (fresh ()
+      (normal 0.0 1.0 x)
+      (normal x 1.0 q))))
+
 (define prog2-proposal
+  (lambda (x q x^ q^)
+    (fresh (b)
+      (conde
+        [(== b #t) (normal 0.0 1.0 x^) (== q q^)]
+        [(== b #f) (== x x^) (normal x 1.0 q^)])
+      )))
+
+(define prog2-proposal-c
   (lambda (x q x^ q^)
     (fresh (b)
 
@@ -108,6 +129,40 @@
          (prog2-mh x q x^^ q^^ density-xq density-xq^)
          (minuso len 1 len-1)
          (prog2-chain len-1 x^^ q^^ d))])))
+
+(define prog2-density-c
+  (lambda (total-density q x)
+    (fresh (dx)
+      (normal-density x 0.0 1.0 dx)
+      (fresh (dq)
+        (normal-density q x 1.0 dq)
+        (pluso dq dx total-density)))))
+
+(define prog2-mh-c
+  (lambda (x q x^^ q^^ density-xq density-xq^)
+    (fresh (x^ q^ b ratio accept)
+      (prog2-proposal x q x^ q^)
+      (prog2-density density-xq  x  q )
+      (prog2-density density-xq^ x^ q^)
+      (/o density-xq^ density-xq ratio)
+      (mino 1.0 ratio accept)
+      (flip accept b)
+      (conde
+        [(== b #t) (== x^ x^^) (== q^ q^^)]
+        [(== b #f) (== x  x^^) (== q  q^^)]))))
+
+(define prog2-chain-c
+  (lambda (len x q ls)
+    (conde
+      [(== 0 len) (== '() ls)]
+      [(== 1 len) (== (list (list x q)) ls)]
+      [(>o len 1 #t)
+       (fresh (d density-xq density-xq^ x^^ q^^ len-1)
+         (== `((,x ,q) . ,d) ls)
+         (prog2-mh x q x^^ q^^ density-xq density-xq^)
+         (minuso len 1 len-1)
+         (prog2-chain len-1 x^^ q^^ d))])))
+
 
 (test-random "prog2-chain-1"
   (run 1 (ls)
